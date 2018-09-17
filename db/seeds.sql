eea2f620611156397daa3613d2f825e823999bc4
rmoj <rmoj99@gmail.com>
2018-09-17 19:43:47 -0400

    Added: schema.sql, seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grandpa',false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Papa', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mama', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mozza',false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hero', false);
+
+
