b78e394790a371c903b5dcdff21a3a2323a13b77
rmoj <rmoj99@gmail.com>
2018-09-17 18:51:10 -0400

    Updated: orm.insertOne()

--- config/orm.js
+++ config/orm.js
@@ -1,27 +1,35 @@
 var connection = require('./connection.js');
 
 var orm = {
-  selectWhere: function(tableInput, colToSearch, valOfCol) {
-    var queryString = 'SELECT * FROM ?? WHERE ?? = ?';
-    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(
+  selectAll: function(table) {
+    var queryString = 'SELECT * FROM ??';
+    connection.query(queryString, [table], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+
+  insertOne: function(table, col1, col2, valOfCol1, valOfCol2) {
+    var queryString = 'INSERT INTO ?? (??,??) VALUES (?,?)';
+    connection.query(
+      queryString,
+      [table, col1, col2, valOfCol1, valOfCol2],
+      function(err, result) {
+        if (err) throw err;
+        console.log(result);
+      }
+    );
+  },
+
+  updateOne: function(table, col, valOfCol, id) {
+    var queryString = 'UPDATE ?? SET ?? = ? WHERE id = ?';
+    connection.query(queryString, [table, col, valOfCol, id], function(
       err,
       result
     ) {
       if (err) throw err;
       console.log(result);
     });
-  },
-
-  update: function(tableInput, colToSearch, valOfCol, id) {
-    var queryString = 'UPDATE ?? SET ?? = ? WHERE id = ?';
-    connection.query(
-      queryString,
-      [tableInput, colToSearch, valOfCol, id],
-      function(err, result) {
-        if (err) throw err;
-        console.log(result);
-      }
-    );
   }
 };
 
