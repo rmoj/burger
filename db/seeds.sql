295eee72f842c8965b23e56111be76f41c77d73d
rmoj <rmoj99@gmail.com>
2018-09-17 20:21:54 -0400

    Update seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,5 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Grandpa',false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Papa', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Mama', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Mozza',false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Hero', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grandpa Burger',false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hero Burger', false);
 
 
