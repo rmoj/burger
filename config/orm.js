82099bf56b23d72c2e9185d1bbd38ba4a3ab1855
rmoj <rmoj99@gmail.com>
2018-09-18 13:30:42 -0400

    Fixed orm function definitions by adding callbacks

--- config/orm.js
+++ config/orm.js
@@ -1,34 +1,34 @@
 var connection = require('./connection.js');
 
 var orm = {
-  selectAll: function(table) {
+  selectAll: function(table, cb) {
     var queryString = 'SELECT * FROM ??';
     connection.query(queryString, [table], function(err, result) {
       if (err) throw err;
-      console.log(result);
+      cb(result);
     });
   },
 
-  insertOne: function(table, col1, col2, valOfCol1, valOfCol2) {
+  insertOne: function(table, col1, col2, valOfCol1, valOfCol2, cb) {
     var queryString = 'INSERT INTO ?? (??,??) VALUES (?,?)';
     connection.query(
       queryString,
       [table, col1, col2, valOfCol1, valOfCol2],
       function(err, result) {
         if (err) throw err;
-        console.log(result);
+        cb(result);
       }
     );
   },
 
-  updateOne: function(table, col, valOfCol, id) {
+  updateOne: function(table, col, valOfCol, id, cb) {
     var queryString = 'UPDATE ?? SET ?? = ? WHERE id = ?';
     connection.query(queryString, [table, col, valOfCol, id], function(
       err,
       result
     ) {
       if (err) throw err;
-      console.log(result);
+      cb(result);
     });
   }
 };
